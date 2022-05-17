2975c00c48292e3a74ec31f1cc28cb194bf487c1
ShamodGeevinda <e18397@eng.pdn.ac.lk>
2022-05-17 15:27:59 +0530

    pairing done

--- app/src/main/java/com/nsnsolutions/test1/PairView.java
+++ app/src/main/java/com/nsnsolutions/test1/PairView.java
@@ -9,10 +9,13 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.ListView;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 import java.util.Set;
@@ -23,7 +26,8 @@ public class PairView extends AppCompatActivity {
     ArrayAdapter<String> arrayAdapter;
     ArrayList arrayList = new ArrayList();
 
-    Set<BluetoothDevice> availableDevices ;
+    //Set<BluetoothDevice> availableDevices ;
+     ArrayList<BluetoothDevice> availableDevices = new ArrayList<BluetoothDevice>();
     ListView pairList;
     Button pair_bt;
 
@@ -40,6 +44,25 @@ public class PairView extends AppCompatActivity {
 
         arrayAdapter = new ArrayAdapter<String>(getApplicationContext(), android.R.layout.simple_expandable_list_item_1, arrayList);
         pairList.setAdapter(arrayAdapter);
+        pairList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                //Intent intent = new Intent(MainActivity.this,ChatView.class);
+                //startActivity(new Intent(MainActivity.this,ChatView.class));
+                //Object obj = adapterView.getItemAtPosition(i);
+               // String val = obj.toString();
+                BluetoothDevice blt = availableDevices.get(i);
+                //intent.putExtra("chater", val);
+                //intent.putExtra("deviceDetail",blt);
+                blt.createBond();
+
+                Log.d("pairing",blt.getName() + blt.getAddress() + blt.getBondState());
+
+                //startActivity(intent);
+
+            }
+
+        });
 
 
 
@@ -65,7 +88,7 @@ public class PairView extends AppCompatActivity {
 
                 if(arrayList.contains(device.getName())){}
                 else {
-
+                    availableDevices.add(device);
                     arrayList.add(device.getName());
                     arrayAdapter.notifyDataSetChanged();
 
